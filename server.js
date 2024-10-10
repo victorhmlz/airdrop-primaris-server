3254c4abeec48105800b65c827a034048691cbd5
victorhmlz <victorhmlz@gmail.com>
2024-10-10 21:18:57 +0200

    Solving vercel error

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://airdrop.metaversalwar.com'], // Agrega aquí los dominios permitidos
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use(express.json());
 
 // Conectar a MongoDB
